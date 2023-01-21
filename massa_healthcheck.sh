260d156366afcb534cf06ab99822e9f134280f40
Ivan Pohosov <ivan.pohosov@dev.pro>
2023-01-21 22:55:13 +0200

    fix script

--- massa_healthcheck.sh
+++ massa_healthcheck.sh
@@ -6,8 +6,9 @@ cd $HOME
 while true
 do
         printf "Check massa logs"
-        if [ $(journalctl -n 5 -u massa --no-pager | grep -c "INFO massa_final_state::final_state: final_state hash at slot") -ne 0 ]; then
+        if [ $(journalctl -n 5 -u massa --no-pager | grep -c "INFO massa_final_state::final_state: final_state hash at slot") < 1 ]; then
 		    printf "Restart massa"
+            printf "\n"
             systemctl restart massa
 	    fi
 
