828406cc023f71d748a3123996148b7bab4b4755
Ivan Pohosov <ivan.pohosov@dev.pro>
2023-01-23 14:15:39 +0200

    fix scripts

--- ironfish_quests.sh
+++ ironfish_quests.sh
@@ -23,26 +23,26 @@ function GetBalanceFunc() {
 
 
 function MintFunc() {
-    RESULT=$(echo "Y" | ${BIN} wallet:mint --name=${GRAFFITI} --metadata=${GRAFFITI}  --amount=1000 --fee=0.00000001) &>/dev/null
+    RESULT=$(echo "Y"  &>/dev/null | ${BIN} wallet:mint --name=${GRAFFITI} --metadata=${GRAFFITI}  --amount=1000 --fee=0.00000001)
     CheckResultFunc "MINT" "${RESULT}"
 }
 
 
 function BurnFunc() {
-    RESULT=$(echo "Y" | ${BIN} wallet:burn --assetId=${IDENTIFIER} --amount=500 --fee=0.00000001) &>/dev/null
+    RESULT=$(echo "Y"  &>/dev/null | ${BIN} wallet:burn --assetId=${IDENTIFIER} --amount=500 --fee=0.00000001)
     CheckResultFunc "BURN" "${RESULT}"
 }
 
 
 function SendFunc() {
     ADDRESS_TO_SEND="dfc2679369551e64e3950e06a88e68466e813c63b100283520045925adbe59ca"
-    RESULT=$(echo "Y" | ${BIN} wallet:send --assetId=${IDENTIFIER} --amount 500 --to ${ADDRESS_TO_SEND} --memo "${GRAFFITI}" --fee=0.00000001) &>/dev/null 
+    RESULT=$(echo "Y"  &>/dev/null | ${BIN} wallet:send --assetId=${IDENTIFIER} --amount 500 --to ${ADDRESS_TO_SEND} --memo "${GRAFFITI}" --fee=0.00000001) &>/dev/null 
     CheckResultFunc "SEND" "${RESULT}"
 }
 
 
 function FaucetFunc() {
-    RESULT=$(echo $IRONFISH_EMAIL | ${BIN} faucet) &>/dev/null
+    RESULT=$(echo $IRONFISH_EMAIL  &>/dev/null | ${BIN} faucet) &>/dev/null
     CheckResultFunc "FAUCET" "${RESULT}"
 }
 
