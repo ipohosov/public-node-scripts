d61efba3c55b7f587d7739df93e0859d811d1335
Ivan Pohosov <ivan.pohosov@dev.pro>
2023-01-23 19:26:02 +0200

    add bundlr cases for reboting the server

--- bundlr_healthcheck.sh
+++ bundlr_healthcheck.sh
@@ -7,6 +7,12 @@ do
         if [ $(docker logs --tail=20 validator | grep -c "Connection reset by peer") -gt 0 ]; then
             printf "Restart bundlr ...................................\n"
             docker-compose -f $HOME/bundlr/validator-rust/docker-compose.yml restart
+        elif [ $(docker logs --tail=20 validator | grep -c "Invalid HTTP version specified") -gt 0 ]; then
+            printf "Restart bundlr ...................................\n"
+            docker-compose -f $HOME/bundlr/validator-rust/docker-compose.yml restart
+        elif [ $(docker logs --tail=20 validator | grep -c "Invalid Header provided") -gt 0 ]; then
+            printf "Restart bundlr ...................................\n"
+            docker-compose -f $HOME/bundlr/validator-rust/docker-compose.yml restart
 	    fi
 
         date=$(date +"%H:%M")
