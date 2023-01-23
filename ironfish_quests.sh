5ced70ba052f117a73ab44e343b08ab8b5b1d7fa
Ivan Pohosov <ivan.pohosov@dev.pro>
2023-01-23 14:00:16 +0200

    fix scripts

--- ironfish_quests.sh
+++ ironfish_quests.sh
@@ -43,6 +43,7 @@ function SendFunc() {
 
 function FaucetFunc() {
     RESULT=$(echo $IRONFISH_EMAIL | ${BIN} faucet &>/dev/null)
+    echo -e "${RESULT}"
     CheckResultFunc "FAUCET" "${RESULT}"
 }
 
