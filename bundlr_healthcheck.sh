b0f199a75e56cf75f23a4286707f9b3e99adf52e
Ivan Pohosov <ivan.pohosov@dev.pro>
2023-01-25 15:50:20 +0200

    update bundlr_healthcheck  script

--- bundlr_healthcheck.sh
+++ bundlr_healthcheck.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+function restart_bundlr() {
+    echo -e "Restart bundlr ...................................\n"
+    docker-compose -f $HOME/bundlr/validator-rust/docker-compose.yml restart
+}
+
 cd $HOME
 while true
 do
-        printf "Check bundlr validator logs \n"
+        echo -e "Check bundlr validator logs \n"
         if [ $(docker logs --tail=20 validator | grep -c "Connection reset by peer") -gt 0 ]; then
-            printf "Restart bundlr ...................................\n"
-            docker-compose -f $HOME/bundlr/validator-rust/docker-compose.yml restart
+            restart_bundlr
         elif [ $(docker logs --tail=20 validator | grep -c "Invalid HTTP version specified") -gt 0 ]; then
-            printf "Restart bundlr ...................................\n"
-            docker-compose -f $HOME/bundlr/validator-rust/docker-compose.yml restart
+            restart_bundlr
         elif [ $(docker logs --tail=20 validator | grep -c "Invalid Header provided") -gt 0 ]; then
-            printf "Restart bundlr ...................................\n"
-            docker-compose -f $HOME/bundlr/validator-rust/docker-compose.yml restart
+            restart_bundlr
 	    fi
 
         date=$(date +"%H:%M")
