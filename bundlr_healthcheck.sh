db5c4e49f2ab3dba9829305664e2ece230276883
Ivan Pohosov <ivan.pohosov@dev.pro>
2023-01-26 12:55:39 +0200

    fix bundlr healthcheck script

--- bundlr_healthcheck.sh
+++ bundlr_healthcheck.sh
@@ -9,11 +9,11 @@ cd $HOME
 while true
 do
         echo -e "Check bundlr validator logs \n"
-        if [ $(docker logs --tail=20 validator | grep -c "Connection reset by peer") -gt 0 ]; then
+        if [ $(docker logs --tail=20 validator 2>&1 | grep -c "Connection reset by peer") -gt 0 ]; then
             restart_bundlr
-        elif [ $(docker logs --tail=20 validator | grep -c "Invalid HTTP version specified") -gt 0 ]; then
+        elif [ $(docker logs --tail=20 validator 2>&1 | grep -c "Invalid HTTP version specified") -gt 0 ]; then
             restart_bundlr
-        elif [ $(docker logs --tail=20 validator | grep -c "Invalid Header provided") -gt 0 ]; then
+        elif [ $(docker logs --tail=20 validator 2>&1 | grep -c "Invalid Header provided") -gt 0 ]; then
             restart_bundlr
 	    fi
 
