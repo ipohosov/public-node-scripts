189d5487b97afd9dc96757dc8958ffd66a6168c1
Ivan Pohosov <ivan.pohosov@dev.pro>
2023-02-02 23:27:43 +0200

    add_massa_install script

--- massa/install_massa.sh
+++ massa/install_massa.sh
@@ -12,31 +12,13 @@ function delete {
 }
 
 function install {
-  wget https://github.com/massalabs/massa/releases/download/TEST.18.0/massa_TEST.19.0_release_linux.tar.gz
+  wget https://github.com/massalabs/massa/releases/download/TEST.19.0/massa_TEST.19.0_release_linux.tar.gz
   tar zxvf massa_TEST.19.0_release_linux.tar.gz -C $HOME/
 }
 
 function routable_ip {
   sed -i 's/.*routable_ip/# \0/' "$HOME/massa/massa-node/base_config/config.toml"
   sed -i "/\[network\]/a routable_ip=\"$(curl -s ifconfig.me)\"" "$HOME/massa/massa-node/base_config/config.toml"
-}
-
-function replace_bootstraps {
-  	config_path="$HOME/massa/massa-node/base_config/config.toml"
-  	bootstrap_list=`wget -qO- https://raw.githubusercontent.com/SecorD0/Massa/main/bootstrap_list.txt | shuf -n50 | awk '{ print "        "$0"," }'`
-  	len=`wc -l < "$config_path"`
-  	start=`grep -n bootstrap_list "$config_path" | cut -d: -f1`
-  	end=`grep -n "\[optionnal\] port on which to listen" "$config_path" | cut -d: -f1`
-  	end=$((end-1))
-  	first_part=`sed "${start},${len}d" "$config_path"`
-  	second_part="
-      bootstrap_list = [
-  ${bootstrap_list}
-      ]
-  "
-  	third_part=`sed "1,${end}d" "$config_path"`
-  	echo "${first_part}${second_part}${third_part}" > "$config_path"
-  	sed -i -e "s%retry_delay *=.*%retry_delay = 10000%; " "$config_path"
 }
 
 function massa_pass {
