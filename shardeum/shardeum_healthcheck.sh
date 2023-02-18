c1fd173bb61822a074fd7de913d391469d89aecf
Ivan Pohosov <ivan.pohosov@dev.pro>
2023-02-18 10:56:39 +0200

    add shrardeum_healthcheck script

---
+++ shardeum/shardeum_healthcheck.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+function login() {
+    TOKEN=$(curl --location --insecure --request POST 'https://${IP_ADDRESS}:${SHARDEUM_DASHBOARD_PORT}/auth/login' \
+    --header 'Content-Type: application/json' \
+    --data-raw '{"password":"${SHARDEUM_DASHBOARD_PWD}"}')
+    echo "${TOKEN}"
+}
+
+
+function get_status() {
+    STATUS=$(docker exec -t shardeum-dashboard operator-cli status | grep state | awk '{ print $2 }')
+    echo "${BINARY}"
+}
+
+
+function start_node() {
+    TOKEN=${1}
+    curl --location --request POST 'https://${IP_ADDRESS}:${SHARDEUM_DASHBOARD_PORT}/api/node/start' \
+    --header 'Content-Type: application/json' \
+    --header 'X-Api-Token: ${TOKEN}'
+}
+
+cd "$HOME" || exit
+IP_ADDRESS=$(wget -qO- http://ipecho.net/plain | xargs echo)
+while true
+do
+        printf "Check shardeum node status \n"
+        STATUS=$(get_status)
+        if [ "$STATUS" = "stopped" ]; then
+            printf "Start shardeum node and wait 5 minutes"
+            TOKEN=$(login)
+            start_node "${TOKEN}"
+            sleep 5m
+        else
+            date=$(date +"%H:%M")
+            echo "Last Update: ${date}"
+            printf "Sleep 15 minutes\n"
+            sleep 15m
+        fi
+done
