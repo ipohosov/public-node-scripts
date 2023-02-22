749abb2c722cbda204fd4b15126a767acfe428d2
Ivan Pohosov <ivan.pohosov@dev.pro>
2023-02-23 01:34:50 +0200

    Add docker file to lamina1

--- lamina1/install_lamina1.sh
+++ lamina1/install_lamina1.sh
@@ -6,6 +6,8 @@ cd lamina1
 
 wget https://raw.githubusercontent.com/ipohosov/public-node-scripts/main/lamina1/Dockerfile
 
+docker build -f docker/Dockerfile -t lamina1-node:latest .
+
 docker run -d --restart unless-stopped \
               --name lamina1-node-testnet \
               --mount source=lamina1-testnet-data,target=/data \
