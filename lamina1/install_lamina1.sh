c6909c87600e5ea441bbec96547be8dc14946fa6
Ivan Pohosov <ivan.pohosov@dev.pro>
2023-02-26 14:45:19 +0200

    update lamina script

--- lamina1/install_lamina1.sh
+++ lamina1/install_lamina1.sh
@@ -4,29 +4,10 @@ mkdir lamina1
 cd lamina1
 
 wget https://raw.githubusercontent.com/ipohosov/public-node-scripts/main/lamina1/Dockerfile
-wget https://raw.githubusercontent.com/ipohosov/public-node-scripts/main/lamina1/Dockerfile.main
 wget https://raw.githubusercontent.com/ipohosov/public-node-scripts/main/lamina1/docker-compose.yaml
 
 IP_ADDRESS=$(wget -qO- http://ipecho.net/plain | xargs echo)
 
 wget https://lamina1.github.io/lamina1/lamina1.latest.ubuntu-latest.tar.gz &&  tar xvf lamina1.latest.ubuntu-latest.tar.gz && rm -rf lamina1.latest.ubuntu-latest.tar.gz
-wget https://lamina1.github.io/lamina1/config.testnet.tar && tar xvf config.testnet.tar && rm -rf config.testnet.tar
 
-if [ ! -d "/root/.lamina1" ]; then
-  docker-compose up -d lamina1-first-start
-  folder_path="/root/.lamina1"
-
-  while [ ! -d "$folder_path" ]
-  do
-    echo "Waiting for $folder_path to appear..."
-    sleep 1
-  done
-
-  echo "$folder_path has appeared!"
-  docker rm -f lamina1-first-start
-fi
-
-rm -rf lamina1/configs/
-mv configs/ lamina1/
-
-docker-compose up -d lamina1-main
+docker-compose up -d lamina1
