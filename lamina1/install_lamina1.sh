cc7b412faed336678730bbd881f1af2a85139abc
Ivan Pohosov <ivan.pohosov@dev.pro>
2023-02-26 15:04:58 +0200

    update lamina script

--- lamina1/install_lamina1.sh
+++ lamina1/install_lamina1.sh
@@ -8,6 +8,8 @@ wget https://raw.githubusercontent.com/ipohosov/public-node-scripts/main/lamina1
 
 PUBLIC_IP=$(wget -qO- http://ipecho.net/plain | xargs echo)
 
+echo "$PUBLIC_IP"
+
 wget https://lamina1.github.io/lamina1/lamina1.latest.ubuntu-latest.tar.gz &&  tar xvf lamina1.latest.ubuntu-latest.tar.gz && rm -rf lamina1.latest.ubuntu-latest.tar.gz
 
 docker-compose up -d lamina1
