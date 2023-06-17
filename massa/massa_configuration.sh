d8ced6edb1a8e4b6c20b0610fb945f5ccd392c1d
Ivan Pohosov <ivan.pohosov@dev.pro>
2023-06-17 23:48:00 +0300

    Add massa rolls

--- massa/massa_configuration.sh
+++ massa/massa_configuration.sh
@@ -41,35 +41,22 @@ done
 echo "Buy roll"
 ./massa-client --pwd $massa_pass buy_rolls $massa_wallet 1 0  > /dev/null
 
-# Get IP and replace routable ip
-echo "Get IP and replace routable ip"
-ifconfig | grep -q 'inet6'
-if [[ $? -eq 0 ]]; then
-    echo "Get IPv6"
-    server_ip=$(ifconfig | grep inet6 | grep global | awk '{print $2}')
-else
-    echo "Get IPv4"
-    server_ip=$(hostname -I | awk '{print $1}')
-fi
-
-config_path="$HOME/massa/massa-node/base_config/config.toml"
-sed -i 's/.*routable_ip/# \0/' $config_path
-sed -i "/\[network\]/a routable_ip=\"$server_ip\"" $config_path
-
-# Remove bootstrap_whitelist.json
-echo "Remove bootstrap_whitelist.json"
-rm -rf /root/massa/massa-node/base_config/bootstrap_whitelist.json
-
-# Update retry_delay
-echo "Update retry_delay"
-sed -i -e "s%retry_delay *=.*%retry_delay = 10000%; " "$config_path"
-
 # Get node id
-echo "Update retry_delay"
+echo "Get node id"
 node_id=$(./massa-client --pwd $massa_pass get_status | grep "Node's ID" | grep -v "IP address" | awk '{print $3}')
 
+# Get IP
+echo "Get IP"
+ifconfig | grep -q 'inet6'
+if [[ $? -eq 0 ]]; then
+    server_ip=$(ifconfig | grep inet6 | grep global | awk '{print $2}')
+    echo "Get IPv6 $server_ip"
+else
+    server_ip=$(hostname -I | awk '{print $1}')
+    echo "Get IPv4 $server_ip"
+fi
+
 # Register node in discord
-echo "Your ip is $server_ip"
 echo "Enter discord id:"
 read discord_id
 
@@ -81,12 +68,7 @@ tmux kill-session -t rolls  > /dev/null
 tmux kill-session -t massa_healthcheck  > /dev/null
 tmux new-session -d -s massa_healthcheck 'bash <(curl -s https://raw.githubusercontent.com/ipohosov/public-node-scripts/main/massa/massa_healthcheck.sh)'  > /dev/null
 tmux new-session -d -s rolls 'bash <(curl -s https://raw.githubusercontent.com/ipohosov/public-node-scripts/main/massa/rolls.sh)'  > /dev/null
- 
 tmux ls
-
-# Restart massa
-echo "Restart massa and sleep 30s"
-sleep 30s
 
 # Print the node info for botstrap
 echo "Node data for bootstrap"
--- massa/update_massa.sh
+++ massa/update_massa.sh
@@ -5,40 +5,48 @@ function get_env {
 	source $HOME/.cargo/env
 }
 
-function massa_backup {
-	cd $HOME
-	if [ ! -d $HOME/massa_backup20/ ]; then
-		mkdir -p $HOME/massa_backup20
-		cp $HOME/massa/massa-node/config/node_privkey.key $HOME/massa_backup20/
-		cp $HOME/massa/massa-client/wallet.dat $HOME/massa_backup20/
-	fi
-	if [ ! -e $HOME/massa_backup20.tar.gz ]; then
-		tar cvzf massa_backup.tar20.gz massa_backup20
-	fi
-}
-
 function delete {
   sudo systemctl stop massa
-  rm -rf massa_TEST.1*
+  rm -rf massa_TEST.*
   rm -rf $HOME/massa
 }
 
 function install {
-  wget https://github.com/massalabs/massa/releases/download/TEST.20.1/massa_TEST.20.1_release_linux.tar.gz
-  tar zxvf massa_TEST.20.1_release_linux.tar.gz -C $HOME/
-}
+  wget https://github.com/massalabs/massa/releases/download/TEST.23.2/massa_TEST.23.2_release_linux.tar.gz
+  tar zxvf massa_TEST.23.2_release_linux.tar.gz -C $HOME/
+  rm -rf massa_TEST.*
 
-function routable_ip {
-  sed -i 's/.*routable_ip/# \0/' "$HOME/massa/massa-node/base_config/config.toml"
-  sed -i "/\[network\]/a routable_ip=\"$(curl -s ifconfig.me)\"" "$HOME/massa/massa-node/base_config/config.toml"
+function configure {
+	# Get IP and replace routable ip
+	echo "Get IP and replace routable ip"
+	ifconfig | grep -q 'inet6'
+	if [[ $? -eq 0 ]]; then
+		echo "Get IPv6"
+		server_ip=$(ifconfig | grep inet6 | grep global | awk '{print $2}')
+	else
+		echo "Get IPv4"
+		server_ip=$(hostname -I | awk '{print $1}')
+	fi
+
+	config_path="$HOME/massa/massa-node/base_config/config.toml"
+	sed -i 's/.*routable_ip/# \0/' $config_path
+	sed -i "/\[network\]/a routable_ip=\"$server_ip\"" $config_path
+
+	# Remove bootstrap_whitelist.json
+	echo "Remove bootstrap_whitelist.json"
+	rm -rf /root/massa/massa-node/base_config/bootstrap_whitelist.json
+
+	# Update retry_delay
+	echo "Update retry_delay"
+	sed -i -e "s%retry_delay *=.*%retry_delay = 10000%; " "$config_path"
 }
 
 
 get_env
-massa_backup
 delete
 install
-routable_ip
+configure
+
 
 
 sudo systemctl start massa
