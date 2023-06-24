581d0e0954e50fa516d45f61159bd5fe2c02bb60
Ivan Pohosov <ivan.pohosov@dev.pro>
2023-06-24 23:42:01 +0300

    Update biitstrap list

--- massa/massa_healthcheck.sh
+++ massa/massa_healthcheck.sh
@@ -4,12 +4,12 @@ cd "$HOME" || exit
 while true
 do
     printf "Check massa logs \n"
-    if [ "$(journalctl -n 50 -u massa --no-pager | grep -c "final_state hash at slot")" -gt 0 ]; then
+    if [ "$(journalctl -n 50 -u massa --no-pager | grep -c "error")" -gt 5 ]; then
         printf "Restart massa ...................................\n"
         sudo systemctl restart massa
     fi
     date=$(date +"%H:%M")
     echo "Last Update: ${date}"
-    printf "Sleep 15 minutes\n"
-    sleep 15m
+    printf "Sleep 5 minutes\n"
+    sleep 5m
 done
