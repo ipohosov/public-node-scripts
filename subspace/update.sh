8869f7c95408f55bb8d2eefa886fcc51846f6622
Ivan Pohosov <ivan.pohosov@dev.pro>
2023-09-23 03:05:34 +0300

    ubdate subspace install

--- subspace/update.sh
+++ subspace/update.sh
@@ -2,7 +2,7 @@
 sudo systemctl stop subspace
 (crontab -l ; echo "0 * * * * truncate -s 0 \$HOME/.local/share/pulsar/logs/*.log*") | crontab -
 
-URL="https://github.com/subspace/pulsar/releases/download/v0.6.8-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.8-alpha"
+URL="https://github.com/subspace/pulsar/releases/download/v0.6.9-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.9-alpha"
 
 rm -f /usr/local/bin/pulsar
 wget -O pulsar $URL
